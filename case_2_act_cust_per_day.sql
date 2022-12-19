f224c751309a6b7647d4b8f9b2a1015b8d0cd137
DintakurthiKarthik <98520011+DintakurthiKarthik@users.noreply.github.com>
2022-12-19 16:31:55 +0530

    Create case_2_act_cust_per_day.sql

---
+++ case_2_act_cust_per_day.sql
@@ -0,0 +1,13 @@
+with customers_list as (
+  select c.customer_unique_id,o.order_id,o.order_purchase_timestamp from `composed-region-358911.olist.customers_ds` c 
+join `composed-region-358911.olist.orders_ds` o on c.customer_id = o.customer_id
+),
+
+monthly_data as 
+(select order_id, extract(month from order_purchase_timestamp) as month,extract(day from order_purchase_timestamp) as day from `composed-region-358911.olist.orders_ds` 
+where order_purchase_timestamp between "2017-01-01" and "2017-12-31"  
+)
+
+select customer_unique_id,monthly_data.order_id,order_purchase_timestamp,month,monthly_data.day,rank() over(partition by customers_list.customer_unique_id order by customers_list.order_purchase_timestamp) as rank 
+from customers_list join monthly_data on customers_list.order_id=monthly_data.order_id
+order by 4,5,1
